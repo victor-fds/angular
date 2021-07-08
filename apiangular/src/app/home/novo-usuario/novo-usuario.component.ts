c1de894d3574fab7af2606b2b2d2f39b0bd8ed68
Victor Fernandes <victor@takefortytwo.com>
2021-07-08 16:39:25 -0300

    Angular push

---
+++ apiangular/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-novo-usuario',
+  templateUrl: './novo-usuario.component.html',
+  styleUrls: ['./novo-usuario.component.css']
+})
+export class NovoUsuarioComponent implements OnInit {
+
+  nome='';
+  email='';
+  senha='';
+  usuario='';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
